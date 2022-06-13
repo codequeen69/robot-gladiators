1301592b613e4af56b4aa63ab07258f0e1342660
Venus DM <crhowe90@gmail.com>
2022-06-12 22:34:30 -0500

    highscore

--- assets/js/game.js
+++ assets/js/game.js
@@ -190,21 +190,27 @@ var startGame = function () {
 
 //function to end the entire game
 var endGame = function () {
-    //if player is still alive, player wins!
-    if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of" + playerInfo.money + ".");
+    window.alert("The game has now ended. Let's see how you did!");
+    //check local storage for high schore, if it's not there, use 0
+    var highScore = localStorage.getItem ("highScore");
+    if (highScore===null){
+        highScore = 0;
     }
-    else {
-        window.alert("You've lost your robot in battle.");
+    //if player has more money than the high score, player has new high score!
+    if(playerInfo.money> highScore){
+        localStorage.setItem("highScore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    }
+    else{
+        alert(playerInfo.name + " did not beat the high score if" + highscore + ". Maybe next time!");
     }
     //ask player if they'd like to play again
     var playAgainConfirm = window.confirm("Would you like to play again?");
-
-    if (playAgainConfirm) {
-        //restart the game
+    if(playAgainConfirm){
         startGame();
     }
-    else {
+    else{
         window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
 };
